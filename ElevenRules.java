0fb942db4595dad0d6d1319363cbb08015c44da4
CaraMcQuillen <55504520+CaraMcQuillen@users.noreply.github.com>
2020-02-07 09:27:43 -0800

    Add files via upload

---
+++ ElevenRules.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class ElevenRules
+{
+    
+   private int number;
+   
+   public ElevenRules(int n)
+   {
+       number = n;
+    }
+   
+   ArrayList<String> output = new ArrayList<String>();
+   ArrayList<String> minusLast = new ArrayList<String>();
+   
+    public void untilTwoDigit()
+   { 
+       int lastDigit;
+       while(number > 99)
+        { 
+          output.add(String.valueOf(number));  
+          lastDigit = number%10;
+          number = number/10;
+          minusLast.add(String.valueOf(number) + "-" + String.valueOf(lastDigit));
+          number -= lastDigit;
+          
+        }
+       
+        output.add(String.valueOf(number));
+       if(number%11==0)
+           minusLast.add("YES");
+       else 
+           minusLast.add("NO");
+       
+       for(int i = 0; i < minusLast.size(); i++)
+       {
+           System.out.println(minusLast.get(i));
+        }
+        
+       for(int i = 0; i < output.size(); i++)
+       {
+           System.out.println(output.get(i));
+        }
+    }
+    
+   
+    
+}
