34bf56857618683f0fa02913e290ec0fb3b64c47
CaraMcQuillen <55504520+CaraMcQuillen@users.noreply.github.com>
2020-02-28 09:26:09 -0800

    Add files via upload

---
+++ Pets.java
@@ -0,0 +1,42 @@
+
+/**
+ * Write a description of class Dogs here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Pets
+{
+    private String name;
+    private int age;
+    public Pets (String n, int a)
+    {
+        name = n;
+        age = a;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String n)
+    {
+        name = n;
+    }
+    
+    public int getAge()
+    {
+        return age;
+    }
+    
+    public void setAge(int a)
+    {
+        age = a;
+    }
+    
+    public String toString()
+    {
+        return "Name: " + name + " Age: " + age;
+    }
+}
